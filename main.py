478999c89013a4ab8c87ac84a2b4d7f37c5c3ff6
emclam <emclam@ucsc.edu>
2023-12-29 01:35:22 -0800

    /spotlight - bot returns the upcoming spotlight (date, pokemon, bonus)

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import discord
 import os
+import GetEvents
 
 TOKEN = "MTE5MDE2NTgwMDk3MjE4OTY5Ng.GCtUXk.71Vpmq-y8FWD_XEGN9hgLq5LH09MLKPQGQxAbQ"
 
-#client = discord.Client(intents=discord.Intents(68608))
 intents = discord.Intents(1 << 9 | 1 << 10 | 1 << 11 | 1 << 12 | 1 << 13 | 1 << 14 | 1 << 15)
 client = discord.Client(intents=intents)
+
+
+def get_spotlight_hour():
+    date, pokemon, bonus = GetEvents.get_spotlight_hour()
+    resp = f"The next Spotlight will be Tuesday, **{date}** featuring **{pokemon}**" + \
+           f"! The bonus will be **{bonus}**."
+    return resp
 
 
 @client.event
@@ -20,6 +27,11 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+    if message.content == "/spotlight":
+        await message.channel.send(get_spotlight_hour())
+        # await message.channel.send(get_spotlight_hour(), file=discord.File('351.png'))
+
 
 #client.run(os.getenv('TOKEN'))
 client.run(TOKEN)
